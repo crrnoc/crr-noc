6b5c66fbea3b8967685c774cf3230080e2c8f439
crrnoc <crrenoccertificate@gmail.com>
2025-06-22 15:55:12 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-# Install Python dependencies
-pip3 install -r requirements.txt
 
-# Start Node.js server
+# Create virtual environment in .venv folder
+python3 -m venv .venv
+
+# Activate it
+source .venv/bin/activate
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Run the Node server
 node server.js
+
