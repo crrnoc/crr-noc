88da82eab0bb20092ae9a7a0440f6da2facd0e40
crrnoc <crrenoccertificate@gmail.com>
2025-07-01 21:41:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -47,8 +47,12 @@ const sessionStore = new MySQLStore({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
+  port: process.env.MYSQLPORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
+
 
 
 app.use(session({
