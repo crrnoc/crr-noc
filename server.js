95168143f7864d5046c094c0ae05a4d7ddab6cbd
crrnoc <crrenoccertificate@gmail.com>
2025-08-10 11:04:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4521,10 +4521,12 @@ if (template === 'attendance') {
   return {
     mobile: cleanMobile,
     message:
-      `ప్రియమైన తల్లిదండ్రులకు, మీ కుమారుడు/కుమార్తె ${s.name} (Reg.No: ${s.reg_no}) యొక్క ${s.semester} సెమిస్టర్ హాజరు శాతం: ${Math.round(s.percentage)}% దయచేసి మీ పిల్లల నిరంతర హాజరును ఖచ్చితంగా నిర్ధారించండి.
-      SIR RAMALINGA REDDY COLLEGE`
+      `ప్రియమైన తల్లిదండ్రులకు, మీ కుమారుడు/కుమార్తె ${s.name} (Reg.No: ${s.reg_no}) యొక్క ${s.semester} సెమిస్టర్ హాజరు శాతం: ${Math.round(s.percentage)}%\n` +
+      `దయచేసి మీ పిల్లల నిరంతర హాజరును ఖచ్చితంగా నిర్ధారించండి.\n` +
+      `SIR RAMALINGA REDDY COLLEGE`
   };
 }
+
 
  else if (template === 'midmarks') {
   return {
@@ -4588,6 +4590,7 @@ if (template === 'attendance') {
     }
   });
 });
+
 
 
 
